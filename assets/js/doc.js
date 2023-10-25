295a8670cd9d58c448574dbd7857c55876ee30b4
Shubham-391 <subhamy1234@gmail.com>
2023-10-25 16:43:06 +0530

    header

---
+++ assets/js/doc.js
@@ -0,0 +1,10 @@
+let view = document.querySelector(".view");
+// navbar
+function navbar() {
+    view.classList.toggle("end-0");
+    document.querySelector("body").classList.toggle("overflow-hidden");
+};
+function navbar2() {
+    view.classList.toggle("end-0");
+    document.querySelector("body").classList.remove("overflow-hidden");
+};
